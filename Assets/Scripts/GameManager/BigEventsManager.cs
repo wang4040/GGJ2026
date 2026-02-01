947f5da7d9b9011b9a0fc6a56e0c5b8a38cb8eeb
Shuaikx <shuaikx828@gmail.com>
2026-01-31 19:43:37 -0500

    init BigEventsManager

---
+++ Assets/Scripts/GameManager/BigEventsManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class BigEventsManager : MonoBehaviour
+{
+    public static BigEventsManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
